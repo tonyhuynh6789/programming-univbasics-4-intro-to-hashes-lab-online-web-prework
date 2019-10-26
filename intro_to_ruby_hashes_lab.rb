fe4d323a295d1fdc4bcd6fe88bb2674f56ddb6ce
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-26 00:05:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if update_counting_hash[key] =  
+  if update_counting_hash[key]  
     update_counting_hash[key] += 1
   
 end 
